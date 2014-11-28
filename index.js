4f621a261734c1befb556bcf52ffd4cd5887bd44
John Otander <johnotander@gmail.com>
2014-11-28 15:37:14 -0700

    Register csslinting to the app registry

--- index.js
+++ index.js
@@ -3,5 +3,17 @@ var cssLintTree = require('broccoli-csslint');
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-csslint'
+  name: 'ember-cli-csslint',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.registry.add('styles', {
+      name: 'ember-cli-csslint',
+      ext: 'css',
+      toTree: function(tree) {
+        return cssLintTree(tree, {});
+      }
+    });
+  }
 };
